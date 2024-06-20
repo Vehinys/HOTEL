3da60e42a4ff8e342a3dc2f98510f9cdd8c39987
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-06-20 10:03:37 +0200

    ADD CLASS / ATTRIBUT / PROPRIETE / GET / SET

--- Client.php
+++ Client.php
@@ -1,1 +1,39 @@
 <?php
+
+class Client {                                                                         // CREE UNE CLASS APPELE "PAYS" ET VONT STOCKER LES ATTRIBUTS DANS LA CLASS "PAYS"
+
+    private string $nom; 
+    private string $prenom;
+
+
+    function __construct(string $nom, string $prenom)
+    {
+        $this-> nom = $nom; 
+        $this-> prenom = $prenom;
+    }
+
+    // GET 
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+    
+    // SET 
+
+    public function setNom()
+    {
+        return $this->nom;
+    }
+
+    public function setPrenom()
+    {
+        return $this->prenom;
+    }
+
+}
