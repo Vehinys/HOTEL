ecfcfb9b5b958a5f0c402985f4d52b53077965d7
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-06-20 10:59:33 +0200

    ADD CLASS RESERVER + ATTRIBUT / GET / SET

--- Reserver.php
+++ Reserver.php
@@ -1,2 +1,39 @@
 <?php
 
+class Reserver {                                                                         // CREE UNE CLASS APPELE "PAYS" ET VONT STOCKER LES ATTRIBUTS DANS LA CLASS "PAYS"
+
+    private DateTime $dateArrivee; 
+    private DateTime $dateDepart;
+
+
+    function __construct($dateArrivee, $dateDepart)
+    {
+        $this->$dateArrivee = new DateTime($$dateArrivee); 
+        $this->$dateDepart = new DateTime($dateDepart); 
+    }
+
+    // GET 
+
+    public function getDateArrivee()
+    {
+        return $this->dateArrivee;
+    }
+
+    public function getDateDepart()
+    {
+        return $this->dateDepart;
+    }
+    
+    // SET 
+
+    public function setDateArrivee()
+    {
+        return $this->dateArrivee;
+    }
+
+    public function setDateDepart()
+    {
+        return $this->dateDepart;
+    }
+
+}
