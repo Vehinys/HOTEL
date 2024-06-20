25ed9de4157c1baee280c36b37e0408bee1b7e10
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-06-20 09:36:58 +0200

    AJOUT DES INCLUDE

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+// INCLUSION DES CLASSES
+
+include 'Chambre.php';
+include 'Client.php';
+include 'Etat.php';
+include 'Hotel.php';
+include 'Reserver.php';
+
+// CRÉATION DE L'HOTEL AVEC LE NOMBRE DES CHAMBRES + LEURS ETATS
