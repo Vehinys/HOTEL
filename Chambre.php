21fe4ab7e7393fbf445653b8bf7b96c2096e85bd
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-06-20 10:53:21 +0200

    ADD GET / SET / ATTRIBUT POUR LA CLASS CHAMBRE

--- Chambre.php
+++ Chambre.php
@@ -1,2 +1,62 @@
 <?php
 
+class Chambre {                                                                         // CREE UNE CLASS APPELE "PAYS" ET VONT STOCKER LES ATTRIBUTS DANS LA CLASS "PAYS"
+
+    private string $numeroDeChambre; 
+    private string $nombreDeLit;
+    private bool $wifi;
+    private string $prix;
+
+    function __construct(string $numeroDeChambre, string $nombreDeLit, bool $wifi, string $prix)
+    {
+        $this-> numeroDeChambre = $numeroDeChambre; 
+        $this-> nombreDeLit = $nombreDeLit;
+        $this-> wifi = $wifi;
+        $this-> prix = $prix;
+    }
+
+    // GET 
+
+    public function getNumeroDeChambre()
+    {
+        return $this->numeroDeChambre;
+    }
+
+    public function getNombreDeLit()
+    {
+        return $this->nombreDeLit;
+    }
+
+    public function getWifi()
+    {
+        return $this->wifi;
+    }
+
+    public function getPrix()
+    {
+        return $this->prix;
+    }
+
+    // SET 
+
+    public function setNumeroDeChambre()
+    {
+        return $this->numeroDeChambre;
+    }
+
+    public function setNombreDeLit()
+    {
+        return $this->nombreDeLit;
+    }
+
+    public function setWifi()
+    {
+        return $this->wifi;
+    }
+
+    public function setPrix()
+    {
+        return $this->prix;
+    }
+
+}
