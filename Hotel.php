aaa241ac5df01962e77acdb1c4ba176816bb1d0f
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-06-24 09:22:47 +0200

    MAJ DES COMMENTAIRE

--- Hotel.php
+++ Hotel.php
@@ -1,82 +1,70 @@
 <?php
 
-class Hotel {
-    private string $nom;
-    private string $adresse;
-    private string $codePostal;
-    private string $ville;
-    private array  $chambres;
-    private array  $reservations;
-    private array  $clients;
+class Hotel {                                                                                                   // CREE UNE CLASS APPELE "HOTEL" ET VONT STOCKER LES ATTRIBUTS DANS LA CLASS "HOTEL"
+
+    private string $nom;                                                                                        // LE NOM DE L HOTEL
+    private string $adresse;                                                                                    // ADRESSE DE L HOTEL
+    private string $codePostal;                                                                                 // CODE POSTAL DE L HOTEL
+    private string $ville;                                                                                      // VILLE DE L HOTEL
+    private array  $chambres;                                                                                   // TABLEAU VIDE POUR GARDER LES INFORMATIONS DE CHAMBRES
+    private array  $reservations;                                                                               // TABLEAU VIDE POUR GARDER LES INFORMATIONS DE RESERVATIONS
+    private array  $clients;                                                                                    // TABLEAU VIDE POUR GARDER LES INFORMATIONS DE CLIENTS
     
 
-    public function __construct(string $nom, string $adresse, string $codePostal, string $ville) {
-        $this->nom          = $nom;
-        $this->adresse      = $adresse;
-        $this->codePostal   = $codePostal;
-        $this->ville        = $ville;
-        $this->chambres     = [];
-        $this->reservations = [];
-        $this->clients      = [];
+    public function __construct(string $nom, string $adresse, string $codePostal, string $ville) {              // CONSTRUCTEUR DE LA CLASSE HOTEL
+        $this->nom          = $nom;                                                                             // INITIALISE LE NOM DE HOTEL
+        $this->adresse      = $adresse;                                                                         // INITIALISE L ADRESSE DE L HOTEL
+        $this->codePostal   = $codePostal;                                                                      // INITIALISE LE CODE POSTAL DE L HOTEL
+        $this->ville        = $ville;                                                                           // INITIALISE LA VILLE DE L HOTEL
+        $this->chambres     = [];                                                                               // INITIALISE CHAMBRES
+        $this->reservations = [];                                                                               // INITIALISE RESERVATIONS
+        $this->clients      = [];                                                                               // INITIALISE CLIENTS
     }
 
-    public function addChambre(Chambre $chambre) {
-        $this->chambres[] = $chambre;
+    public function addChambre(Chambre $chambre) {                                                              // METHODE POUR AJOUTER UNE CHAMBRE A LA LISTE DES HOTELS
+        $this->chambres[] = $chambre;                                                                           // AJOUTE LES INFORMATIONS DE CHMBRE A LA LISTE
     }
 
-    public function addReservation(Reservation $reservation) {
-        $this->reservations[] = $reservation;
+    public function addReservation(Reservation $reservation) {                                                  // METHODE POUR AJOUTER UNE RESERVATION A LA LISTE DES HOTELS
+        $this->reservations[] = $reservation;                                                                   // AJOUTE LES INFORMATIONS DE CHMBRE A LA LISTE
     }
 
-    public function addClient(client $client) {
-        $this->clients[] = $client;
-    }
-
-    
-    public function calculNbChambres() {
-        return count($this->chambres);
+    public function addClient(client $client) {                                                                 // METHODE POUR AJOUTER UN CLIENT A LA LISTE DES HOTELS
+        $this->clients[] = $client;                                                                             // AJOUTE LES INFORMATIONS DE CHMBRE A LA LISTE
     }
 
     
-    public function nbReservations() {
-        return count($this->reservations);
+    public function calculNbChambres() {                                                                        // FONCTION QUI CALCUL LE NOMBRE DE CHAMBRES PAR HOTEL
+        return count($this->chambres);                                                                        
     }
 
     
-    public function resultChambreLibre() {
+    public function nbReservations() {                                                                          // FONCTION QUI CALCUL LE NOMBRE DE RESERVATION PAR HOTEL
+        return count($this->reservations);                                                                    
+    }
+
+    
+    public function resultChambreLibre() {                                                                      // FONCTION QUI CALCUL LE NOMBRE DE CHAMBRE LIBRE
         return $this->calculNbChambres() - $this->nbReservations();
     }
 
-    private function afficherNbReserClient(Client $client) {
-
-        
+    private function afficherNbReserClient(Client $client) {                                                    // FONCTION QUI CALCUL LE NOMBRE DE RESERVATION PAR CLIENT
         $clientReservations = array_filter($this->reservations, function($reservation) use ($client) {
-            
             return $reservation->getClient() === $client;
         });
 
         $nbReservations = count($clientReservations);
-
-        
-        echo "<div style='width: 100px; padding: 5px 5px; background-color: #32d296; margin: 0 0 20px 0; color: white;'>";
-        
+        echo "<div style='width: 100px; padding: 5px 5px; 
+        background-color: #32d296; margin: 0 0 20px 0; color: white;'>";
         echo "{$nbReservations} Réservations";
-        
         echo "</div>";
     }
 
-
-    
-    public function resultPrix() {
-        
+    public function resultPrix() {                                                                              // FONCTION QUI CALCUL LE PRIX TOTAL PAR LE NOMBRE DE NUIT * LE PRIX
         $totalPrix = 0;
-        
         foreach ($this->reservations as $reservation) {
-            
             $nbNuits = $reservation->getNbNuits();
-            
             $prixParNuit = $reservation->getChambre()->getPrix();
-            
             $totalPrix += $nbNuits * $prixParNuit;
         }
         return $totalPrix;
@@ -84,105 +72,66 @@ class Hotel {
 
     // GET 
 
-    public function getNom() {
+    public function getNom() {                                                                                  // UNE FONCTION QUI RETOURNE LE NOM DE L'HOTEL
         return $this->nom;
     }
 
-    public function getAdresse() {
+    public function getAdresse() {                                                                              // UNE FONCTION QUI RETOURNE LE ADRESSE DE L'HOTEL
         return $this->adresse;
     }
 
-    public function getCodePostal() {
+    public function getCodePostal() {                                                                           // UNE FONCTION QUI RETOURNE LE CODE POSTAL DE L'HOTEL
         return $this->codePostal;
     }
 
-    public function getVille() {
+    public function getVille() {                                                                                // UNE FONCTION QUI RETOURNE LA VILLE DE L'HOTEL
         return $this->ville;
     }
 
-    public function getChambres() {
+    public function getChambres() {                                                                             // UNE FONCTION QUI RETOURNE LE CHAMBRES DE L'HOTEL
         return $this->chambres;
     }
 
-    public function getReservations() {
+    public function getReservations() {                                                                         // UNE FONCTION QUI RETOURNE LA RESERVATION DE L'HOTEL
         return $this->reservations;
     }
 
-    public function getClients() {
+    public function getClients() {                                                                              // UNE FONCTION QUI RETOURNE LE CLIENT DE L'HOTEL
         return $this->clients;
     }
     
-    // SET 
-
-    public function setNom() {
-        return $this->nom;
-    }
-
-    public function setAdresse() {
-        return $this->adresse;
-    }
-
-    public function setCodePostal() {
-        return $this->codePostal;
-    }
-
-    public function setVille() {
-        return $this->ville;
-    }
-
-
-
-    public function afficherInfoHotel() {
+    public function afficherInfoHotel() {                                                                       // UNE FONCTION QUI AFFICHE LES INFORMATIONS DE L'HOTEL = NOM / VILLE / NOMBRE DE TOTAL DE CHAMBRE / NOMBRE DE CHAMBRE RESERVER / NOMBRE DE CHAMBRE DISPONIBLE
 
         echo " <div style='font-size : 15px ;  margin: 10px 0 0 0 ;'>";
-        
         echo "<h2 style='margin : 0;'>  {$this->nom} {$this-> ville}  </h2>";
-        
         echo "{$this-> adresse} {$this-> codePostal}  {$this-> ville}<br><br>";
-        
         echo "Nombre total de chambres : {$this->calculNbChambres()}<br>" ;
-        
         echo "Nombre de chambres réservées : {$this->nbReservations()} <br>"; 
-        
         echo "Nombre de chambres disponibles : {$this->resultChambreLibre()} <br>";
-        
         echo " </div>";
     }
 
-    public function afficherInfoReservation() {
-
+    public function afficherInfoReservation() {                                                                 // UNE FONCTION QUI AFFICHE LES INFORMATIONS DE L'HOTEL
         
         echo " <div style='font-size : 15px ;  margin: 10px 0 0px 0 ;'>";
-        
         echo "<h2 style='margin : 0;'>  Réservations de l'hôtel {$this->nom} {$this->ville}</h2>";
-        
         echo " </div>";
     
-        
-        if ($this->nbReservations() > 0) {
+        if ($this->nbReservations() > 0) {                                                                      // BOUCLE QUI DONNE LE NOMBRE DE RESERVATION TOTAL
             
-            echo " <div style='width : 100px; padding: 5px 5px; background-color : #32d296; color : white;'>";
-            
+            echo " <div style='width : 100px; padding: 5px 5px; background-color : #32d296; color : white;'>"; 
             echo "{$this->nbReservations()} Réservations";
-            
             echo " </div>";
-            
             echo "<div style='margin-top: 20px;'>";
             
-            foreach ($this->reservations as $reservation) {
+            foreach ($this->reservations as $reservation) {                                                     // BOUCLE POUR AFFICHER LA LISTE DES CHAMBRES QUI ONT ETE RESERVER AVEC AFFICHAGE LE CLIENT / LE NOM DE LA CHAMBRE / LA DATE D ARRIVER / LA DATE DU DEPART           
                 
                 $client = $reservation->getClient();
-                
                 $chambre = $reservation->getChambre();
-                
                 echo " <div style='font-size : 15px;'>";
-                
                 echo "{$client->getNom()} {$client->getPrenom()} - Chambre {$chambre->getNomChambre()} - du 
-                
                 {$reservation->getDateArrivee()} au {$reservation->getDateDepart()}</>";
-
                 echo " </div>";
-
             }
 
             echo "</div>";
@@ -190,83 +139,67 @@ class Hotel {
         } else {
             
             echo " <div style='padding: 5px 5px;margin: 0 0 20px 0; font-size : 15px;'>";
-
             echo "Aucune réservation !";
-
             echo " </div>";
-
         }
-
     }
 
-    public function afficherInfoReserClient(Client $client) {
+    public function afficherInfoReserClient(Client $client) {                                                   // UNE FONCTION QUI AFFICHE LES RESERVATIONS DU CLIENT
+
 
         echo "<div style='font-size: 15px;'>";
-
         echo "<h2 style='margin: 0;'>Réservations de {$client->getNom()} {$client->getPrenom()}</h2>";
-
         $this->afficherNbReserClient($client);
 
         $clientReservations = array_filter($this->reservations, function($reservation) use ($client) {
-            
             return $reservation->getClient() === $client;
 
         });
 
-        if (count($clientReservations) > 0) {
+        if (count($clientReservations) > 0) {                                                                   // BOUCLE QUI AFFICHE LES RESERVATIONS DU CLIENT = HOTEL / VILLE / NOM DES CHAMBRE / NB DE LIT / PRIX / WIFI / DATE D ARRIVER ET DE DEPART
 
             foreach ($clientReservations as $reservation) {
-                
                 $chambre = $reservation->getChambre();
-
                 $hotel = $chambre->getHotel();
-                
                 echo "<b>{$hotel->getNom()} {$hotel->getVille()} / </b>";
-
                 echo "Chambre : {$chambre->getNomChambre()} ";
-                
                 echo "( {$chambre->getNombreDeLit()} lits - {$chambre->getPrix()} €" ;
-                
                 echo " - Wifi : " ;
 
                 if ($chambre->getWifi()) {
-
                     echo " Oui";
 
                 } else {
-
                     echo " Non";
                 }
 
                 echo " ) ";
-
                 echo "du {$reservation->getDateArrivee()} au {$reservation->getDateDepart()}<br>";
             }
             $totalPrix = 0;
 
-            foreach ($clientReservations as $reservation) {
-
+            foreach ($clientReservations as $reservation) {                                                     // BOUCLE POUR AFFICHER LE NOMBRE DE CHAMBRE DANS LE TABLEAU
                 $totalPrix += $reservation->getChambre()->getPrix();
             }
             echo "<b>Total : {$this->resultPrix()} €</b>";
+
         } else {
+
             echo "Aucune réservation !<br>";
         }
         echo "</div><br>";
     }
 
-    public function afficherInfoStatut() {
+    public function afficherInfoStatut() {                                                                      // FUNCTION QUI AFFICHE LE STATUT DES CHAMBRES
 
         echo "<div style='font-size: 15px;'>";
-
         echo "<h2 style='margin : 0;'>  Status des Chambres {$this->nom} {$this->ville}</h2>";
-        
         echo "</div><br>";
     }
 
 
-    public function afficherTableau() {
-        echo "<div>";
+    public function afficherTableau() {                                                                         // FUNCTION QUI AFFICHE LE TABLEAU DU STATUT DES CHAMBRES PAR HOTEL
+        echo "<div>";                                                                                           // AFFICHAGE D'UN TABLEAU 
         echo "<style>";
         echo "tr.chambre-row:hover {";
         echo "    background-color: coral;";
@@ -287,7 +220,7 @@ class Hotel {
         echo "</thead>";
         echo "<tbody>";
     
-        foreach ($this->chambres as $chambre) {
+        foreach ($this->chambres as $chambre) {                                                                 // BOUCLE POUR AFFICHER LE NOMBRE DE CHAMBRE DANS LE TABLEAU
             $estReservee = $this->estChambreReservee($chambre);
             $etat = $estReservee ? 'Réservée' : 'Disponible';
             $wifiIcon = $chambre->getWifi();
@@ -302,13 +235,12 @@ class Hotel {
             echo "<td class='etat-cell' style='{$etatStyle}'>{$etat}</td>";
             echo "</tr>";
         }
-    
         echo "</tbody>";
         echo "</table>";
         echo "</div>";
     }
 
-    private function estChambreReservee(Chambre $chambre) {
+    public function estChambreReservee(Chambre $chambre) {                                                      // FUNCTION QUI AFFICHE LE STATUT [SI IL EST DISPONIBLE OU PAS ] DES CHAMBRES PAR HOTEL
         foreach ($this->reservations as $reservation) {
             if ($reservation->getChambre() === $chambre) {
                 return true;
