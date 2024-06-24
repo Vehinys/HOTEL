965dba25446a3f68ff7ea328558e18d8c8f6af53
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-06-24 09:35:38 +0200

    MAJ COMMENTAIRE CLIENT

--- Client.php
+++ Client.php
@@ -1,58 +1,44 @@
 <?php
 
-class Client {                                                                         
+class Client {                                                            // CREE UNE CLASS APPELE "CLIENT" ET VONT STOCKER LES ATTRIBUTS DANS LA CLASS "CLIENT"                                                                         
 
-    private string $nom; 
-    private string $prenom;
-    private hotel  $hotel;
-    private array  $reseverations;
-
-
+    private string $nom;                                                  // LE NOM DU CLIENT
+    private string $prenom;                                               // LE PRENOM DU CLIENT
+    private hotel  $hotel;                                                // L HOTEL CONCERNE PAR LE CLIENT
+    private array  $reservations ;                                        // TABLEAU VIDE POUR GARDER LES INFORMATIONS DE RESERVATIONS
 
 
-    function __construct(string $nom, string $prenom, hotel $hotel) {
-        $this-> nom           = $nom; 
-        $this-> prenom        = $prenom;
-        $this-> hotel         = $hotel;
-        $this-> reservations  = [];
-        $hotel -> addClient($this);
+
+
+    function __construct(string $nom, string $prenom, hotel $hotel) {     // CONSTRUCTEUR DE LA CLASSE HOTEL
+        $this-> nom           = $nom;                                     // INITIALISE LE NOM DU CLIENT
+        $this-> prenom        = $prenom;                                  // INITIALISE LE PRENOM DU CLIENT
+        $this-> hotel         = $hotel;                                   // INITIALISE L HOTEL CONCERNE PAR LE CLIENT
+        $this-> reservations  = [];                                       // 
+        $hotel -> addClient($this);                                       // AJOUTE LES INFORMATIONS CLIENT AU TABLEAU DES CLIENTS DE L HOTEL
 
     }
 
-    public function addReservation(Reservation $reservation) {
-        $this->reservations[] = $reservation;
+    public function addReservation(Reservation $reservation) {            // FONCTION POUR AJOUTER UNE RESERVATIONS A LA LISTE DES RESERVATIONS
+        $this->reservations[] = $reservation;                             // AJOUTE LES INFORMATIONS DE RESERVATIONS A LA LISTE
     }
 
     // GET 
 
-    public function getNom() {
+    public function getNom() {                                            // UNE FONCTION QUI RETOURNE LE NOM DU CLIENT
         return $this->nom;
     }
 
-    public function getPrenom() {
+    public function getPrenom() {                                         // UNE FONCTION QUI RETOURNE LE PRENOM DU CLIENT
         return $this->prenom;
     }
 
-    public function getHotel() {
+    public function getHotel() {                                          // UNE FONCTION QUI RETOURNE LE HOTEL DU CLIENT
         return $this->hotel;
     }
 
-    public function getReservation() {
-        return $this->reservation;
-    }
-
-    // SET 
-
-    public function setNom() {
-        return $this->nom;
-    }
-
-    public function setPrenom() {
-        return $this->prenom;
-    }
-
-    public function setHotel() {
-        return $this->hotel;
+    public function getReservations() {                                   // UNE FONCTION QUI RETOURNE LE RESERVATION DU CLIENT
+        return $this->reservations;
     }
 
 }
